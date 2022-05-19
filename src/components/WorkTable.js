1a0a1898f0958fd07503abc2a49c6320c3bebee0
jahid4striver <jahidhassn701@gmail.comr>
2022-05-19 06:43:46 +0600

    line Through added

--- src/components/WorkTable.js
+++ src/components/WorkTable.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const WorkTable = ({work}) => {
+    const [addClass, setAddClass]= useState(false);
+
+    const handleClass=()=>{
+        setAddClass(true);
+    }
     return (
         <tr>
-            <td>{work.task}</td>
-            <td>{work.description}</td>
-            <td><button className='btn btn-success'>Complete</button></td>
+            <td className={`${ addClass ?'line-through':''}`}>{work.task}</td>
+            <td className={`${ addClass ?'line-through':''}`}>{work.description}</td>
+            <td><button onClick={handleClass} className={`btn btn-success`}>Done</button></td>
             <td><button className='btn btn-danger'>Delete</button></td>
         </tr>
     );
